2621f22af8ee289e37048c719569a53622a22bf0
Steven McGrath <smcgrath@tenable.com>
2013-01-23 17:13:13 -0600

    Adjusted stats Javascript to talk to production instead of dev.

--- theme/static/js/home_stats.js
+++ theme/static/js/home_stats.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$.getJSON('http://dev.bukget.org/stats/todays_trends', function(data){
+	$.getJSON('http://api.bukget.org/stats/todays_trends', function(data){
 		$("#hometotals").append('<p class="centerme">Total Plugins: ' + data.plugin_count + '&nbsp;&nbsp;|&nbsp;&nbsp;Total Versions: ' + data.version_count + '</p><br />');
 		$("#homestats").append('<h2>Todays Top 10 Plugins</h2><hr /><ul>')
 		$.each(data.top_plugs, function(key, item) {
--- theme/static/js/naughty_list.js
+++ theme/static/js/naughty_list.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$.getJSON('http://dev.bukget.org/stats/naughty_list', function(data){
+	$.getJSON('http://api.bukget.org/stats/naughty_list', function(data){
 		$("#naughty").append("<ul>")
 		$.each(data, function(key, item) {
 			$("#naughty").append('<li><em><strong>' + item.slug + '</strong></em>&nbsp;&nbsp;&nbsp;' + item.authors.join(', ') + '</li>');
